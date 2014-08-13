77bb348914c8ea9889805f68d90c92234e10159f
Kyler Schroeder <kylerschroeder@fullsail.edu>
2014-08-12 20:36:48 -0400

    Added Comments

--- W2Expressions/Expressions_Professional/js/main.js
+++ W2Expressions/Expressions_Professional/js/main.js
@@ -11,12 +11,12 @@ var principal = 1;
 var fidelity = 10;
 var greatWest = 4;
 
-alert("Welcome, let's find out how much work you've actually done this week!");
+alert("Welcome, let's find out how much work you've actually done this week!"); // tells the user what we're doing
 
-var numPrincipal = prompt("How many Principal tickets did you close this week?");
-var numFidelity = prompt("How many Fidelity tickets did you close this week?");
-var numGW = prompt("How many Great West tickets did you close this week?");
+var numPrincipal = prompt("How many Principal tickets did you close this week?"); // finds the number to multiply with the principal variable
+var numFidelity = prompt("How many Fidelity tickets did you close this week?"); // finds the number to multiply with the fidelity variable
+var numGW = prompt("How many Great West tickets did you close this week?"); // finds the number to multiply with the greatWest variable
 
-var totalHrs = (principal * numPrincipal) + (fidelity * numFidelity) + (greatWest * numGW);
+var totalHrs = (principal * numPrincipal) + (fidelity * numFidelity) + (greatWest * numGW); // multiplys the user inupt times the appropriate var and adds them all together
 
-alert("You grinded out approximately " + totalHrs + " hours worth of mind-numbing keyboard pounding!");
+alert("You grinded out approximately " + totalHrs + " hours worth of mind-numbing keyboard pounding!"); // displays the result of the artihmetic above
