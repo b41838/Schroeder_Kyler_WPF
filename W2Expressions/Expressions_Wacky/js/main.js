19f0e7f9d0243a73d4540f798b984fb5c28375dd
Kyler Schroeder <kylerschroeder@fullsail.edu>
2014-08-13 20:19:04 -0400

    Wacky Calc
    Started wacky script but i’m going to need to use decisions so I will
    rethink it.

--- W2Expressions/Expressions_Wacky/js/main.js
+++ W2Expressions/Expressions_Wacky/js/main.js
@@ -4,3 +4,15 @@
  * Wacky Calculator
  */
 
+// this script will ask you three simple questions to find out what kind of warrior you are
+
+var mage;
+var knight;
+var archer;
+var ninja;
+
+alert("Lets find out what kind of warrior you are!");
+
+var q1 = prompt("Do you prefer books, television, movies or ghost stories?")
+
+var q2 = prompt("")
