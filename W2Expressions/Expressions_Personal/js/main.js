d62db179488ee47db96d7da219137c26ee6c47de
Kyler Schroeder <kylerschroeder@fullsail.edu>
2014-08-13 21:18:40 -0400

    Added basic concept to personal calc

--- W2Expressions/Expressions_Personal/js/main.js
+++ W2Expressions/Expressions_Personal/js/main.js
@@ -5,3 +5,15 @@
  */
 
 alert("Greets");
+
+
+// No one likes to waste money so this calculator will help me figure out how many kegs I need to buy
+
+var ozInKeg = 1984;
+var numPeople = prompt("How many people will be drinking from the keg?");
+var cupSize = prompt("How many ounces in a cup?");
+var beersPerPerson = prompt("How many beers does each person want to drink?");
+
+var totalOz = numPeople * beersPerPerson * cupSize;
+
+alert(totalOz);
