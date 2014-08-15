c03bd8ef49ac7f55971efc92c7824119fcc23bc8
Kyler Schroeder <kylerschroeder@fullsail.edu>
2014-08-14 20:54:03 -0400

    tried to use an if statement
    result is always first answer in statement.  scrapping..  remembered at
    the last second that i needed == instead of =.  now i’m gold.

--- W2Expressions/Expressions_Wacky/js/main.js
+++ W2Expressions/Expressions_Wacky/js/main.js
@@ -6,22 +6,45 @@
 
 // this script will ask you three simple questions to find out what kind of warrior you are
 
-var mage;
+/* var mage;
 var knight;
 var archer;
-var ninja;
+var ninja; */
 
-var attributes[4];
+var attributes = ["mage", "knight", "archer", "ninja"];
 
 alert("Lets find out what kind of warrior you are!");
 
-var attributes[0] = prompt("Do you prefer books, television, movies or ghost stories?")
+var a = prompt("Do you prefer books, television, movies or ghost stories?");
 
-var attributes[1] = prompt("Would you rather go to the past, present or future?")
+var b = prompt("Would you rather go to the past, present or future?");
 
-var attributes[2] = prompt("Black, Blue, Brown or White?")
+var c = prompt("Black, Blue, Brown or White?");
 
-var attributes[3] = prompt("What is your weapon of choice?  Sword, Gun or Trickery?")
+var d = prompt("What is your weapon of choice?  Sword, Gun or Trickery?");
 
-var attributes[4] = prompt("")
+var total = 0;
+// var attributes[4] = prompt("");
 
+/*if (a = "books") {
+		total = total + 1;
+	} else if (a = "television") {
+		total = total + 2;
+	} else if (a = "movies") {
+		total = total + 3;
+	} else {
+		total = total + 4;
+	}*/
+	
+if (a == 1) {
+		total = total + 1;
+	} else if (a == 2) {
+		total = total + 2;
+	} else if (a == 3) {
+		total = total + 3;
+	} else {
+		total == total + 4;
+	}
+
+
+console.log (total);
