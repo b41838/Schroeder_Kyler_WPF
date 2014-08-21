65dec36cb2c5cb2adb2b06da0977cd29984d3275
Kyler Schroeder <kylerschroeder@fullsail.edu>
2014-08-21 18:55:26 -0400

    Variables - Professional
    All variables and prompts are defined.

--- W3Conditionals/Conditionals_Professional/js/main.js
+++ W3Conditionals/Conditionals_Professional/js/main.js
@@ -10,3 +10,27 @@
 // DISCLAIMER: This calculation is not real, we use a calculator provided by the Department of Labor.
 
 alert("Greets");
+
+
+var janRate = .03;
+var febRate = .06;
+var marRate = .05;
+var aprRate = .03;
+var mayRate = .02;
+var junRate = .04;
+var julRate = .03;
+var augRate = .07;
+var sepRate = .07;
+var octRate = .05;
+var novRate = .03;
+var decRate = .03;
+var month = propmt("What month were the earnings lost for?");
+var daysOutstanding = prompt("How many days in " + month + " were missed?");
+var totalPrincipal = prompt("What was the total lost principal?");
+
+console.log(month);
+console.log(daysOutstanding);
+console.log(totalPrincipal);
+
+// alert("Let/'s calculate some lost earnings!");
+
