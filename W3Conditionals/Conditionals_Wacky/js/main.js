bb3f688c69e7af1ff5da63dd2c4dd93809eee13f
Kyler Schroeder <kylerschroeder@fullsail.edu>
2014-08-21 20:54:44 -0400

    code works - wacky
    had a = instead of ==…again

--- W3Conditionals/Conditionals_Wacky/js/main.js
+++ W3Conditionals/Conditionals_Wacky/js/main.js
@@ -11,14 +11,14 @@ alert("Greets");
 
 var age = prompt("How many months old is your Pitbull?");
 var active = prompt("How would you rate your dogs energy level: active, berzerk or insane?");
-var foodAmount = ["One cup per day", "One and a half cups per day", "Two cups per day", "Two and a half cups per day"]
+var foodAmount = ["One cup per day", "One and a half cups per day", "Two cups per day", "Two and a half cups per day"];
 
 console.log(age);
 console.log(active);
 
-/*if (age <= 12 && active == "active") {
+if (age <= 12 && active == "active") {
 		console.log(foodAmount[0]);
-	} else if (age <= 12 && active == "berzerk" || active = "insane") {
+	} else if (age <= 12 && active == "berzerk" || active == "insane") {
 		console.log(foodAmount[1]);
 	} else if (age > 12 && age <= 48 && active == "active" || active == "berzerk") {
 		console.log(foodAmount[2]);
@@ -29,5 +29,3 @@ console.log(active);
 	} else {
 		console.log(foodAmount[2]);
 	};
-
-*/
