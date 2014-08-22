c4c60d40bdcd0212ea89be520fc98e30d0e4e87d
Kyler Schroeder <kylerschroeder@fullsail.edu>
2014-08-21 20:27:27 -0400

    Initial commit - Wacky
    wrote variables and (not working) logic

--- W3Conditionals/Conditionals_Wacky/js/main.js
+++ W3Conditionals/Conditionals_Wacky/js/main.js
@@ -8,3 +8,22 @@
 // I have a 1~ year old pitbull and he eats.. A LOT.  This calculator will figure how much food to give your dog so it's satisfied and doesn't become a fatty.
 
 alert("Greets");
+
+var age = prompt("How many months old is your Pitbull?");
+var active = prompt("How would you rate your dogs energy level: active, berzerk or insane?");
+var foodAmount = ["One cup per day", "One and a half cups per day", "Two cups per day", "Two and a half cups per day"]
+
+if (age <= 12) && active == "active") {
+		console.log(foodAmount[0];
+	} else if (age <= 12) && active == "berzerk" || active = "insane") {
+		console.log(foodAmount[1];
+	} else if (age > 12) && age <= 48 && active == "active" || active == "berzerk") {
+		console.log(foodAmount[2];
+	} else if (age > 12) && age <= 48 && active == "insane") {
+		console.log(foodAmount[3];
+	} else if (age > 48) && active == "active" || active == "berzerk") {
+		console.log(foodAmount[1];
+	} else {
+		console.log(foodAmount[2]);
+	};
+
