400e1c8006029f36b1ac40fbee694298e95013c5
Kyler Schroeder <kylerschroeder@fullsail.edu>
2014-08-21 20:31:21 -0400

    adjusted and added checks
    had ill-placed parens and added console logs for variables

--- W3Conditionals/Conditionals_Wacky/js/main.js
+++ W3Conditionals/Conditionals_Wacky/js/main.js
@@ -13,17 +13,21 @@ var age = prompt("How many months old is your Pitbull?");
 var active = prompt("How would you rate your dogs energy level: active, berzerk or insane?");
 var foodAmount = ["One cup per day", "One and a half cups per day", "Two cups per day", "Two and a half cups per day"]
 
-if (age <= 12) && active == "active") {
-		console.log(foodAmount[0];
-	} else if (age <= 12) && active == "berzerk" || active = "insane") {
-		console.log(foodAmount[1];
-	} else if (age > 12) && age <= 48 && active == "active" || active == "berzerk") {
-		console.log(foodAmount[2];
-	} else if (age > 12) && age <= 48 && active == "insane") {
-		console.log(foodAmount[3];
-	} else if (age > 48) && active == "active" || active == "berzerk") {
-		console.log(foodAmount[1];
+console.log(age);
+console.log(active);
+
+/*if (age <= 12 && active == "active") {
+		console.log(foodAmount[0]);
+	} else if (age <= 12 && active == "berzerk" || active = "insane") {
+		console.log(foodAmount[1]);
+	} else if (age > 12 && age <= 48 && active == "active" || active == "berzerk") {
+		console.log(foodAmount[2]);
+	} else if (age > 12 && age <= 48 && active == "insane") {
+		console.log(foodAmount[3]);
+	} else if (age > 48 && active == "active" || active == "berzerk") {
+		console.log(foodAmount[1]);
 	} else {
 		console.log(foodAmount[2]);
 	};
 
+*/
