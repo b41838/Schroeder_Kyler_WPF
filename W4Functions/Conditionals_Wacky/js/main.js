2e3c738efe10c2662c592836794ac35396db6689
Kyler Schroeder <kylerschroeder@fullsail.edu>
2014-08-28 18:55:16 -0400

    first function and variables
    added variables for weapons and added first function

--- W4Functions/Conditionals_Wacky/js/main.js
+++ W4Functions/Conditionals_Wacky/js/main.js
@@ -7,10 +7,23 @@
 
 // Coming through like a wrecking ball!  A game where you will break down a wall, causing the lowest amount of colateral damage.
 
-alert("Greets");
+// alert("Greets");
 
 var wall = Math.floor(Math.random() * (25 - 1 + 1)) + 1;
+var blue = 3;
+var red = 5;
+var black = 10;
 
-console.log(wall);
+//console.log(wall);
 
 alert("Whoa!  That wall looks like it needs to be destroyed!  It has " + wall + " hit points, time to get wrecking!");
+alert("You have three wepons, the blue wrecking ball does " + blue + " damage, the red ball does " + red + " damage and the black ball does " + black + " damage.");
+
+
+function wreck() {
+	var round1 = prompt("Would you like to use the blue, red or black ball first?");
+	return round1;
+};
+
+
+console.log(round1);
