a490e7f8187b131c6e0e2b55014453528ba9d9dd
Kyler Schroeder <kylerschroeder@fullsail.edu>
2014-08-28 18:37:37 -0400

    math.random
     trying to get a random number for my wall variable

--- W4Functions/Conditionals_Wacky/js/main.js
+++ W4Functions/Conditionals_Wacky/js/main.js
@@ -5,4 +5,10 @@
  * Wacky Calculator
  */
 
+// Coming through like a wrecking ball!  A game where you will break down a wall, causing the lowest amount of colateral damage.
+
 alert("Greets");
+
+var wall = Math.Random(0,25);
+
+console.log(wall);
