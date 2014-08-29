4bd22b628ba2ef0f9077e687b512284e024e8a57
Kyler Schroeder <kylerschroeder@fullsail.edu>
2014-08-28 20:16:41 -0400

    added explanation and variables to professional
    variables are entered by the user

--- W4Functions/Conditionals_Professional/js/main.js
+++ W4Functions/Conditionals_Professional/js/main.js
@@ -5,6 +5,16 @@
  * Professional Calculator
  */
 
-// I work for a Payroll Software company
+// I work for a Payroll Software company and deal with the data contained in copanies 401k files. This will show how much you accrue over time.
 
-alert("Greets");
+// alert("Greets");
+
+alert("Let's figure out how much money you will have in your retirement over time!");
+
+var pretax = prompt("how much money per pay period will you withold for 401k?");
+var posttax = prompt("How much money per pay period will you contribute for your Roth?");
+var match = prompt("What percent does your employer match?");
+
+console.log(pretax);
+console.log(posttax);
+console.log(match);
